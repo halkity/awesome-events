068cd347a9e829722a826a7cfb3651f89ac7786c
halkity <saito75061@gmail.com>
2021-01-08 13:16:57 +0900

    EventDBを反映

--- db/migrate/20210108035429_create_events.rb
+++ db/migrate/20210108035429_create_events.rb
@@ -1,13 +1,12 @@
 class CreateEvents < ActiveRecord::Migration[6.0]
   def change
     create_table :events do |t|
-      t.bigint :owner_id
-      t.string :name
-      t.string :place
-      t.datetime :stard_at
-      t.datetime :end_at
-      t.text :content
-
+      t.bigint :owner_id    , null: felse
+      t.string :name        , null: felse
+      t.string :place       , null: felse
+      t.datetime :stard_at  , null: felse
+      t.datetime :end_at    , null: felse
+      t.text :content       , null: felse
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_06_045527) do
-
-  create_table "events", force: :cascade do |t|
-    t.bigint "owner_id", null: false
-    t.string "name", null: false
-    t.string "place", null: false
-    t.datetime "stard_at", null: false
-    t.datetime "end_at", null: false
-    t.text "content", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["owner_id"], name: "index_events_on_owner_id"
-  end
+ActiveRecord::Schema.define(version: 2021_01_03_013908) do
 
   create_table "users", force: :cascade do |t|
     t.string "provider", null: false
