6ffe01dbd2fc5218c58756d32012b17d5a922dea
halkity <saito75061@gmail.com>
2021-01-08 13:25:24 +0900

    Eventモデルのバリデーションを追加
    ・name,place,contentに文字制限と未記入制限
    ・stard_atとend_atに未記入制限
    ・stard_at >= end_atの制限

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,18 @@
 class Event < ApplicationRecord
+  validates :name, length: {maximum: 50}, presence:true
+  validates :place, length: {maximum: 50}, presence:true
+  validates :content, length: {maximum: 50}, presence:true
+  validates :stard_at, presence:true
+  validates :end_at, presence:true
+  validates :stard_at_shouldbe_before_end_at
+
+  private
+
+  def stard_at_shouldbe_before_end_at
+    return unless stard_at && end_at
+
+    if stard_at >= end_at
+      errors.add(:stard_at, "は終了時間よりも前に設定してください")
+    end
+  end
 end
