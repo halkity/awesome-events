89b7ca404d95e5b89525326a16196bf2d2bb4cb7
halkity <saito75061@gmail.com>
2021-01-09 14:42:51 +0900

    Eventモデルのstard_at_shouldbe_before_end_atを修正

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,13 +4,12 @@ class Event < ApplicationRecord
   validates :content, length: {maximum: 50}, presence:true
   validates :stard_at, presence:true
   validates :end_at, presence:true
-  validates :stard_at_shouldbe_before_end_at
+  validate :stard_at_shouldbe_before_end_at
 
   private
 
   def stard_at_shouldbe_before_end_at
     return unless stard_at && end_at
-
     if stard_at >= end_at
       errors.add(:stard_at, "は終了時間よりも前に設定してください")
     end
