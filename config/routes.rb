5c82128300b4a3ff7cb0e901fd56e04318ccd653
halkity <saito75061@gmail.com>
2021-01-14 15:50:35 +0900

    退会機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get '/auth/:provider/callback', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-
+  delete '/logout', to: 'sessions#destroy', as: :logout
+  resources :retirements
   resources :events do
     resources :tickets
   end
+
 end
