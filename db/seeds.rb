69261410d1a9a007385da27a90447f05c1ab55dd
halkity <saito75061@gmail.com>
2021-01-14 11:25:28 +0900

    サンプル作成
    ・Userの作成
    ・イベントの作成
    ・イベント参加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,56 +1,77 @@
+# イベントカテゴリー
+event_name_array = [
+  "ランニング",
+  "テニス",
+  "サッカー",
+  "ゴルフ",
+  "野球",
+  "読書",
+  "映画",
+  "ファッション",
+  "勉強会",
+  "飲食パーティー",
+  "朝活",
+]
+
+# 参加コメント
+comment = [
+  "参加します！",
+  "一緒にやりたいです",
+  "暇なので",
+  "( ^ω^ )",
+  "初心者ですがよろしくお願いします！",
+  "滑り込み参加",
+  "さ・ん・か・し・ま・す",
+]
+
 # ユーザー作成
-99.times do |n|
+30.times do |n|
   user = Faker::Omniauth.github
-  User.create!{
+  User.create!(
     provider: user[:provider],
     uid: user[:uid],
     name: user[:info][:nickname],
     image_url: user[:info][:image]
-  }
+  )
 end
 
+
 # イベント作成
-users = User.order(:created_at).take(6)
-10.times do |n|
-  users.each_with_index do |user, idx|
+users = User.order(:created_at).take(20)
+20.times do |n|
+  users.each do |user|
+    # event_arrayからランダムで一つ取り出す
+    event_name = event_name_array.sample
+    # 月日時分をランダムで取得
+    event_month = rand(1..12)
+    event_day = rand(1..27)
+    event_hour = rand(0..22)
+    event_minute = rand(0..5) * 10
+    # イベント作成処理
     user.created_events.create!(
-      case n
-      when 0 then name: "ランニング#{idx + 1}"
-      when 1 then name: "テニス#{idx+1}"
-      when 2 then name: "サッカー#{idx+1}"
-      when 4 then name: "野球#{idx+1}"
-      when 5 then name: "読書#{idx+1}"
-      when 6 then name: "映画#{idx+1}"
-      when 7 then name: "ファッション#{idx+1}"
-      when 8 then name: "勉強#{idx+1}"
-      when 9 then name: "パーティー#{idx+1}"
-      end
-      place: Faker::Address.state
-      start_at: DateTime.new(2022, 12, n, 17)
-      end_at: DateTime.new(2022, 12, n+1, 17)
+      name: event_name,
+      place: Faker::Address.state,
+      start_at: DateTime.new(2022, event_month, event_day, event_hour, event_minute),
+      end_at: DateTime.new(2022, event_month, event_day, event_hour + 1, event_minute),
       content: "参加してください"
     )
   end
 end
 
 # 参加のリレーション
-
 # ランダムにユーザーを20件複数取得
-# ランダムにユーザーを複数取得
-
-# user_random_ids =(User.pluck(:id).sample(10))
-# event_random_ids = Event.pluck(:id)
-# events = Event.where.not()
-#   users.each do |user|
-#     user.tickets.build do |t|
-#       t.event = event
-#       t.comment = params[:ticket][:comment]
-#     end
-#   end
-
-# following = users[2..50]
-# followers = users[3..40]
-# following.each { |followed| user.follow(followed) }
-# followers.each { User.pluck(:id)
-# users = User.find |follower| follower.follow(user) }
-
+users = User.all.sample(20).sort
+# ユーザーを各個取り出す
+users.each do |user|
+  # イベント取得
+  if join_event_ids = user.tickets.includes(:event).pluck(:id)
+    event = Event.where.not("id IN (?) AND owner_id = ?", join_event_ids, user.id).sample
+  else
+    event = Event.where.not("owner_id = ?", user.id).sample
+  end
+  # イベントへの参加処理
+  user.tickets.create! do |t|
+    t.event = event
+    t.comment = comment.sample
+  end
+end
