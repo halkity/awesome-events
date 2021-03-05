1be51dd64b3ee2cba9f483a26e0f95c021403bab
halkity <saito75061@gmail.com>
2021-03-05 09:09:59 +0900

    イベントnewのフォーム登録テスト

--- test/sign_in_helper.rb
+++ test/sign_in_helper.rb
@@ -4,16 +4,26 @@ module SignInHelper
     OmniAuth.config.add_mock(
       user.provider,
       uid: user.uid,
-      info: { nickname: user.name, image: user.image_url }
-    )
-    visit root_url
-    click_on "GitHubでログイン"
+      info: { nickname: user.name,
+              image: user.image_url })
+    case
+    when respond_to?(:visit)
+      visit root_url
+      click_on "GitHubでログイン"
+    when respond_to?(:get)
+      get "/auth/github/callback"
+    else
+      raise NotImplementedError.new
+    end
     @current_user = user
   end
-  
+
   def current_user
     @current_user
   end
-  
-  
+end
+
+  class ActionDispatch::IntegrationTest #((2))
+    include SignInHelper #((3))
+  end
 end
